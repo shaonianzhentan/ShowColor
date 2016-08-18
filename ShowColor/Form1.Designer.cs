c436b7fccf629d0052a487dad4ff90a07d741d48
shaonianzhentan <shaonianzhentan@qq.com>
2016-08-18 16:55:03 +0800

    获取选择文本里的十六进制颜色编码

---
+++ ShowColor/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace ShowColor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
+            this.listView1 = new System.Windows.Forms.ListView();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // fileSystemWatcher1
+            // 
+            this.fileSystemWatcher1.EnableRaisingEvents = true;
+            this.fileSystemWatcher1.SynchronizingObject = this;
+            // 
+            // listView1
+            // 
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.listView1.Location = new System.Drawing.Point(0, 0);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(495, 263);
+            this.listView1.TabIndex = 0;
+            this.listView1.TileSize = new System.Drawing.Size(85, 26);
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Tile;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(495, 263);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "显示颜色——作者QQ：635147515";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.IO.FileSystemWatcher fileSystemWatcher1;
+        private System.Windows.Forms.ListView listView1;
+
+
+    }
+}
+
---
+++ ShowColor/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using GoodHelper.WinAPIHelper;
+using System.Threading;
+using System.Text.RegularExpressions;
+
+namespace ShowColor
+{
+  
+    public partial class Form1 : Form
+    {
+        protected override void WndProc(ref Message m)
+        {
+            base.WndProc(ref m);
+            switch (m.Msg)
+            {
+                case WindowsAPI.WM_HOTKEY:
+                    switch ((int)m.WParam)
+                    {
+                        case 1991:
+                            WindowsAPI.keybd_event(WindowsAPI.VK_CONTROL, 0, 0, 0);
+                            WindowsAPI.keybd_event(WindowsAPI.VK_C, 0, 0, 0);
+                            WindowsAPI.keybd_event(WindowsAPI.VK_C, 0, 2, 0);
+                            WindowsAPI.keybd_event(WindowsAPI.VK_CONTROL, 0, 2, 0);
+                            Thread.Sleep(100);
+                            string color = Clipboard.GetText();
+                            Regex reg = new Regex("#(([0-9a-fA-F]{6})|([0-9a-fA-F]{3}))");
+                            listView1.Items.Clear();
+                            foreach (Match match in reg.Matches(color)) {
+                                try
+                                {
+                                    ListViewItem lvi = new ListViewItem();
+                                    lvi.Text = match.Value;
+                                    lvi.ForeColor = Color.White;
+                                    lvi.BackColor = ColorTranslator.FromHtml(match.Value);                                    
+                                    
+                                    listView1.Items.Add(lvi);
+                                }
+                                catch (Exception) { 
+
+                                }
+                            }                            
+                            WindowsAPI.SetForegroundWindow(this.Handle);
+                            break;
+                    }
+                    break;
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();            
+        }
+      
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            WindowsAPI.RegisterHotKey(this.Handle, 1991, WindowsAPI.None, WindowsAPI.VK_F1);
+        }
+    }
+}
